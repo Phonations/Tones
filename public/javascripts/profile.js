4a93d14b69fa45f98a7737d2f865c1ad6ebea2ea
Thibaut Delille <thibaut@jvst.us>
2013-05-05 21:05:58 -0700

    left nav in profile working

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-
+	$('.profile-sidenav li').bind('click', function(){
+		if(!$(this).hasClass('active')){
+			$('.profile-sidenav .active').removeClass('active');
+			$(this).addClass('active');
+			$('.span9 .module').css('display', 'none');
+			$('.span9 .module.'+$(this).attr('id')).css('display', 'block');
+		}
+	})
 });
